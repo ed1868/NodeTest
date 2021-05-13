65be893955d3df9c40fe1eb7f625836b08f8e2b3
ed1868 <ruizeduardo21@gmail.com>
2021-05-13 16:20:48 -0400

    query model done

---
+++ src/models/queries.model.ts
@@ -0,0 +1,14 @@
+// USING MONGOOSE MODEL SCHEMAS
+import { model, Schema, Document } from 'mongoose';
+import { Query } from '@interfaces/query.interface';
+
+const querySchema: Schema = new Schema({
+  query: {
+    type: String,
+    required: true,
+  },
+});
+
+const queryModel = model<Query & Document>('Query', querySchema);
+
+export default queryModel;
