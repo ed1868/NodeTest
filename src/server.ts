9732417ae02dc08c51985326180f0a000ba2e9b5
ed1868 <ruizeduardo21@gmail.com>
2021-05-14 17:17:18 -0400

    final code cleanup

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,13 @@ process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
+
 import validateEnv from '@utils/validateEnv';
 import QueryRoute from '@routes/query.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new QueryRoute()]);
+const app = new App([new IndexRoute(), new QueryRoute()]);
 
 app.listen();
